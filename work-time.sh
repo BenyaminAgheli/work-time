66e822437f00cf21dc1df08213d7c790eb30439e
BenyaminAgheli <aghelibenyamin1@gmail.com>
2026-02-10 12:55:51 +0330

    add week/month filter

--- work-time.sh
+++ work-time.sh
@@ -1,39 +1,59 @@
 #!/bin/bash
 
+# Defaults
 IS_JALALI=false
+EXPORT_CSV=false
+TOTAL_HOURS=0
+FILTER_DATE="0000-00-00"
+
+# Argument Parsing
 while [[ $# -gt 0 ]]; do
     case $1 in
         -j) IS_JALALI=true; shift ;;
-        *) shift ;; # Ignore other arguments
+        --csv) EXPORT_CSV=true; shift ;;
+        -w) FILTER_DATE=$(date -d "1 week ago" +%Y-%m-%d); shift ;;
+        -m) FILTER_DATE=$(date -d "1 month ago" +%Y-%m-%d); shift ;;
+        *) shift ;;
     esac
 done
 
 get-date() {
-    journalctl --list-boots --no-pager | awk '
-   	NR == 1 { next }
-   	{
-        d=$4; 
-        s=$3" "$4" "$5; 
-        e=$9; 
-        if(!(d in f)) {
-            f[d]=s; 
-            o[++c]=d
+    # We pass the FILTER_DATE into awk to handle the filtering manually
+    journalctl --list-boots --no-pager | awk -v filter="$FILTER_DATE" '
+        NR == 1 { next }
+        {
+            # $4 is the date in YYYY-MM-DD format
+            if ($4 < filter) next;
+
+            d=$4; 
+            s=$3" "$4" "$5; 
+            e=$9; 
+            if(!(d in f)) {
+                f[d]=s; 
+                o[++c]=d
+            } 
+            l[d]=e
         } 
-        l[d]=e
-    } 
-    END {
-        for(i=1;i<=c;i++) print f[o[i]] " " l[o[i]]
-    }'
+        END {
+            for(i=1;i<=c;i++) print f[o[i]] " " l[o[i]]
+        }'
 }
 
-echo "+------------+----------+----------+-------+--------------------------------+"
-echo "|    DATE    |    IN    |    OUT   | HOURS |           VISUAL CHART         |"
-echo "+------------+----------+----------+-------+--------------------------------+"
+# Header Handling
+if [ "$EXPORT_CSV" = false ]; then
+    echo "+------------+----------+----------+-------+--------------------------------+"
+    echo "|    DATE    |    IN    |    OUT   | HOURS |            VISUAL CHART        |"
+    echo "+------------+----------+----------+-------+--------------------------------+"
+else
+    echo "Date,In,Out,Hours"
+fi
 
-get-date | while read -r DAY DATE IN OUT; do
+while read -r DAY DATE IN OUT; do
+    
     if [ "$IS_JALALI" = true ]; then
         DATE=$(jdate -j ${DATE//\-/\/} +"%Y/%m/%d")
     fi
+
     start_sec=$(date -d "1970-01-01 $IN" +%s 2>/dev/null)
     end_sec=$(date -d "1970-01-01 $OUT" +%s 2>/dev/null)
 
@@ -42,23 +62,25 @@ get-date | while read -r DAY DATE IN OUT; do
         if [ $diff_sec -lt 0 ]; then diff_sec=$((diff_sec + 86400)); fi
 
         hours=$(awk "BEGIN {printf \"%.2f\", $diff_sec/3600}")
+        TOTAL_HOURS=$(awk "BEGIN {printf \"%.2f\", $TOTAL_HOURS + $hours}")
 
-        # Calculate bar count (1 block per 30 mins)
-        # We use a check to ensure bar_count is at least 0
         bar_count=$(awk "BEGIN {bc=int($hours * 2); print (bc>0?bc:0)}")
-
-        if [ "$bar_count" -gt 0 ]; then
-            bar=$(printf "%${bar_count}s" | tr ' ' '#')
-        else
-            bar=""
-        fi
+        bar=$(printf "%${bar_count}s" | tr ' ' '#')
     else
-        # This handles lines that don't fit the time format
         continue
     fi
 
-    printf "| %-10s | %-8s | %-5s | %-5s | %-30s |\n" "$DATE" "$IN" "$OUT" "$hours" "$bar"
+    if [ "$EXPORT_CSV" = true ]; then
+        echo "$DATE,$IN,$OUT,$hours"
+    else
+        printf "| %-10s | %-8s | %-8s | %-5s | %-30s |\n" "$DATE" "$IN" "$OUT" "$hours" "$bar"
+    fi
 
-done
+done < <(get-date)
 
-echo "+------------+----------+----------+-------+--------------------------------+"
+# Footer
+if [ "$EXPORT_CSV" = false ]; then
+    echo "+------------+----------+----------+-------+--------------------------------+"
+    printf "| TOTAL ACCUMULATED HOURS: %-44s     |\n" "$TOTAL_HOURS"                      
+    echo "+------------+----------+----------+-------+--------------------------------+"
+fi
